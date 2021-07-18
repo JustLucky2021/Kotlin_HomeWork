f56ccec29129e178bc3e517d60453c65d99f9fba
JustLucky2021 <just_lucky2007@mail.ru>
2021-07-18 16:28:53 +0300

    HW lesson2 kotlin

--- app/src/main/java/com/example/kotlin_1_hw/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_1_hw/MainActivity.kt
@@ -1,31 +1,26 @@
 package com.example.kotlin_1_hw
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val LOG_MARKER = "lesson_2_kotlin"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val Random_Range = (0..100).random()
-        val pi = 3.14
-        Log.d("kotlin_1_lesson", "Random_range value is : $Random_Range")
-        val Circle_Area = Random_Range * Random_Range * pi
-        val Square_Area = Random_Range * Random_Range
-        when (Circle_Area > Square_Area) {
-            true -> {
-                Log.d("kotlin_1_lesson", "Circle_Area = $Circle_Area")
-                Log.d("kotlin_1_lesson", "Square_Area = $Square_Area")
-                Log.d("kotlin_1_lesson", "Circle_Area more then Square_Area")
+        val animals = listOf(Cat(), Dog())
+        for (animal in animals) {
+            if (animal is Dog) {
+                animal.bite()
             }
-            false -> {
-                Log.d("kotlin_1_lesson", "Circle_Area = $Circle_Area")
-                Log.d("kotlin_1_lesson", "Square_Area = $Square_Area")
-                Log.d("kotlin_1_lesson", "Square_Area more then Circle_Area")
+            if (animal is Cat) {
+                animal.scratch()
             }
-
+            animal.voice()
         }
+
     }
 }
