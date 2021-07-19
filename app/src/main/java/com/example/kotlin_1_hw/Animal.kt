731b0754062f1b91074396029f27149c7577b802
JustLucky2021 <just_lucky2007@mail.ru>
2021-07-19 23:14:48 +0300

    delete classes and fix personInCurrentAge

--- app/src/main/java/com/example/kotlin_1_hw/Animal.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlin_1_hw
-
-import android.util.Log
-
-open class Animal(val name: String = "Animal name", val age: Int = 0){
-    open fun voice(){
-        Log.d("lesson_2_kotlin", "Animal voice must be here")
-    }
-}
--- app/src/main/java/com/example/kotlin_1_hw/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_1_hw/MainActivity.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-
-fun generateRandomAge(minAge: Int, maxAge: Int): Int{
-    val randomAge = (minAge..maxAge).random()
-    return randomAge
-}
-
-fun List<String>.random(): String {
-    val randomIndex = (0..this.lastIndex).random()
-    return this[randomIndex]
-}
-
-
 class MainActivity : AppCompatActivity() {
     companion object {
         val LOG_MARKER = "lesson_2_kotlin"
+
+        fun generateRandomAge(minAge: Int, maxAge: Int): Int{
+            val randomAge = (minAge..maxAge).random()
+            return randomAge
+        }
+
+        fun List<String>.random(): String {
+            val randomIndex = (0..this.lastIndex).random()
+            return this[randomIndex]
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +25,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val listOfNames = listOf<String>("Денис", "Ирина", "Татьна", "Дмитрий", "Светозар",
                                           "Елена", "Николай", "Лидия", "Надежда", "Александр");
-        //val somePerson1 = Person(name = listOfNames.random(), age = generateRandomAge(20, 40))
         val personList = mutableListOf<Person>()
-        for (i in 0..10){
-            personList.add(Person(name = listOfNames.random(), age = generateRandomAge(minAge = 18, maxAge = 55)))
-        }
-        //Log.d("$LOG_MARKER", "$personList")
+        for (i in 0..10)
+            personList.add(Person
+                                (name = listOfNames.random(),
+                                age = generateRandomAge(minAge = 18, maxAge = 55)))
         personList.toList()
         personList.forEach{ Log.d("$LOG_MARKER",  "${personList.indexOf(it)}  $it ")}
         val personListFilter = personList.filter{it.age in 18..40}
@@ -39,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         personListFilter.forEach{ Log.d("$LOG_MARKER",  "${personListFilter.indexOf(it)}  $it ")}
         val personInCurrentAge = personList.find { it.age == 32 }
         if(personInCurrentAge !== null)
-            Log.d("$LOG_MARKER", "personInCurrent")
+            Log.d("$LOG_MARKER", "personInCurrent is $personInCurrentAge")
         else
             Log.d("$LOG_MARKER", "Person in current range didn't find.")
     }
